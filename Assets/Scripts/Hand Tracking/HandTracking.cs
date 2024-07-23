0d58b78776aaeff6e7d545c4d3b45c2e9eb1eeef
timpt0261 <rchavez0215@gmail.com>
2024-07-23 14:44:04 -0700

    Updated HandTracking
    - Hand tracking is able to detected gesture at a higher probability than before

--- Assets/Scripts/Hand Tracking/HandTracking.cs
+++ Assets/Scripts/Hand Tracking/HandTracking.cs
@@ -11,33 +11,30 @@ public class HandTracking : MonoBehaviour
     void Update()
     {
         string data = udpReceive.data;
-        if (data == "None" || data.Length<2) { return; }
+        if (data == "None" || data.Length < 2) { return; }
 
 
 
         data = data.Remove(0, 1);
-        data = data.Remove(data.Length-1, 1);
-    
+        data = data.Remove(data.Length - 1, 1);
+
         string[] points = data.Split(',');
         Debug.Log(points);
-        print($"points, {points.Length}, {points[points.Length-1]}");
+        print($"points, {points.Length}, {points[points.Length - 1]}");
 
 
-        for (int i = 0; i < points.Length - 1; i+=3) 
+        for (int i = 0; i < points.Length - 1; i += 3)
         {
-            float x = 3 - float.Parse(points[i])/100;
-            float y = float.Parse(points[i + 1])/100;
-            float z = float.Parse(points[i + 2])/100;
+            float x = 3 - float.Parse(points[i]) / 100;
+            float y = float.Parse(points[i + 1]) / 100;
+            float z = float.Parse(points[i + 2]) / 100;
 
-            handPoints[i/3].transform.localPosition = new Vector3(x, y, z);
+            handPoints[i / 3].transform.localPosition = new Vector3(x, y, z);
         }
 
-        
-        if(int.Parse(points[points.Length - 1])==1){
-            img_has_thumb_up = true;
-        }
+        img_has_thumb_up = int.Parse(points[points.Length - 1]) == 1;
 
-        Debug.Log("img_has_thumb_up: "+img_has_thumb_up);
+        Debug.Log("img_has_thumb_up: " + img_has_thumb_up);
 
         // thumbs up or not is detected...
     }
