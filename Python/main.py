562ab64364fe6cba65281e64e49355f2dbdef921
timpt0261 <rchavez0215@gmail.com>
2024-07-23 13:13:50 -0700

    Created venv to run python scripts

--- Python/main.py
+++ Python/main.py
@@ -8,7 +8,7 @@ from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 
 # Create an GestureRecognizer object.
-base_options = python.BaseOptions(model_asset_path='gesture_recognizer.task')
+base_options = python.BaseOptions(model_asset_path='Python\gesture_recognizer.task')
 options = vision.GestureRecognizerOptions(base_options=base_options)
 recognizer = vision.GestureRecognizer.create_from_options(options)
 
